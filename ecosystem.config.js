7cb6e8a19b5591e64329e7feb889a6ced30cb75a
benjamalegni <lukabenjaminmalegni@gmail.com>
2026-03-28 19:56:57 -0300

    add ecosystem.config.js for pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       cwd: '/home/luka/Documents/chamuyame',
       env: {
         NODE_ENV: 'production',
-        GROQ_API_KEY: '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',
+        GROQ_API_KEY: 'XXX',
         GROQ_API_URL: 'https://api.groq.com/openai/v1/chat/completions',
         GROQ_MODEL: 'groq/compound-mini',
         SYSTEM_PROMPT: 'Sos un argentino que ayuda a responder mensajes de chat. REGLAS: Maximo 2 oraciones. Tono natural argentino, como un wsp normal. Sin emojis, sin formalidades. Si no hay contexto, responde tranqui.',
